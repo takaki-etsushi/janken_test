dcb14e0e98733ac418ab70ba00a9472264a534de
EtsushiTakaki <e.takaki.sys24@morijyobi.ac.jp>
2024-12-12 00:10:06 +0900

    test_computer.py#2

--- tests/test_computer.py
+++ tests/test_computer.py
@@ -1,1 +1,19 @@
 import unittest
+from unittest.mock import patch
+from source.computer import computer_pon
+
+class TestComputerPon(unittest.TestCase):
+    @patch("random.choice", return_value="グー")
+    def test_random_choice_1(self, mock_choice):
+        result = computer_pon()
+        self.assertEqual(result, "グー")
+
+    @patch("random.choice", return_value="チョキ")
+    def test_random_choice_2(self, mock_choice):
+        result = computer_pon()
+        self.assertEqual(result, "チョキ")
+
+    @patch("random.choice", return_value="パー")
+    def test_random_choice_3(self, mock_choice):
+        result = computer_pon()
+        self.assertEqual(result, "パー")
